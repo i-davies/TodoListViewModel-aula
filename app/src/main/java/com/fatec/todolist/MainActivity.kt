2c57dcf437686e5d18670613688e20bd6d5c5e8d
Icaro Davies <idavies@outlook.com>
2026-04-30 19:14:52 -0300

    fix: row MainActivity

--- app/src/main/java/com/fatec/todolist/MainActivity.kt
+++ app/src/main/java/com/fatec/todolist/MainActivity.kt
@@ -11,37 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.fatec.todolist.ui.screens.TaskScreen
 import com.fatec.todolist.ui.theme.TodoListViewModelTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            TodoListViewModelTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodoListViewModelTheme {
-        Greeting("Android")
+        super.onCreate(savedInstanceState)
+        setContent { TaskScreen() }
     }
 }
